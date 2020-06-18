9d767f325bf2601cb67008b223035e0c4ee872a1
sps2001 <67009293+sps2001@users.noreply.github.com>
2020-06-18 18:48:25 +0530

    Add files via upload

---
+++ home.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#simple calculator
+a=float(input("value of a "))
+b=float(input("value of b "))
+print("sum of two numbers=",a+b)
+print("difference of two numbers",a-b)
+print("product of two numbers",a*b)
+print("division of two numbers",a/b)
+print("modules of two numbers",a%b)
+print("exponential of two numbers",a**b )
+
+
+# In[ ]:
+
+
+
+
